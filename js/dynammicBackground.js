70b4d7fc24d05819e45d3caf485c3c9b80c69a1c
Isabella Ramirez <irborrero@gmail.com>
2020-05-15 01:25:39 -0400

    updated background images

--- js/dynammicBackground.js
+++ js/dynammicBackground.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     var home = document.getElementById('home');
     var how_it_works = document.getElementById('how_it_works')
+    var what_we_do = document.getElementById('what_we_do')
+    var contact = document.getElementById('contact')
 
     var backgrounds = new Array( 'url(images/home/titulo5.png)','url(images/home/titulo9.jpg)');
 
@@ -18,6 +20,14 @@ $(document).ready(function(){
 
     home.style.background = 'url(images/home/titulo5.png)';
     home.style.backgroundSize= "cover";
-    how_it_works.style.background = 'url(images/home/titulo5.png)';
+    how_it_works.style.background = 'url(images/platfromBlack.png)' ;
     how_it_works.style.backgroundSize= "100vw";
+    how_it_works.style.backgroundAttachment = "fixed";
+    contact.style.background = 'url(images/contact-us.png)'
+    contact.style.backgroundSize= "100vw";
+    contact.style.backgroundAttachment = "fixed";
+    what_we_do.style.background = 'url(images/platfromBlack.png)' ;
+    what_we_do.style.backgroundSize= "100vw";
+    what_we_do.style.backgroundAttachment = "fixed"
+
 });
