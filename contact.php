0d818318fd035423163775623edc730fe68389e0
Isabella Ramirez <irborrero@gmail.com>
2020-05-25 15:09:25 +0100

    debugging

--- contact.php
+++ contact.php
@@ -1,7 +1,6 @@
 <?php
 if(isset($_POST['submit'])){
 
-    echo 'hello';
 
     $to = "irborrero@gmail.com"; // this is your Email address
     $from = "irborrero@gmail.com"; // this is the sender's Email address
@@ -15,13 +14,22 @@ if(isset($_POST['submit'])){
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
 
-    mail($to,$subject,$message,$headers);
+    if(mail($to,$subject,$message,$headers))
+    {
+        echo "Message accepted";
+    }
+    else
+    {
+        echo "Error: Message not accepted";
+    }
+
+    /*
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
+    */
 
-
-    header('Location: index0.html');
+    //header('Location: index0.html');
 
 }
 ?>
