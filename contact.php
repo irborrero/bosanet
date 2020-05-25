6c256ba6ff27229e822bf49547554da7d9b66b7c
Isabella Ramirez <irborrero@gmail.com>
2020-05-25 15:00:25 +0100

    si funciona esto lloro

--- contact.php
+++ contact.php
@@ -2,8 +2,8 @@
 if(isset($_POST['submit'])){
 
     echo 'hello';
-    /*
-    $to = "irborrero@gmail..com"; // this is your Email address
+
+    $to = "irborrero@gmail.com"; // this is your Email address
     $from = "irborrero@gmail.com"; // this is the sender's Email address
     $first_name = "ISABELLA";
     $last_name = "RAMIREZ";
@@ -19,7 +19,7 @@ if(isset($_POST['submit'])){
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
-    */
+
 
     header('Location: index0.html');
 
