3ca320e677a8d136fb2470169657fde242b742b3
Isabella Ramirez <irborrero@gmail.com>
2020-05-25 15:15:38 +0100

    trying to add names

--- contact.php
+++ contact.php
@@ -1,34 +1,14 @@
 <?php
 if(isset($_POST['submit'])){
 
-
-    $to = "irborrero@gmail.com"; // this is your Email address
-    $from = "irborrero@gmail.com"; // this is the sender's Email address
-    $first_name = "ISABELLA";
-    $last_name = "RAMIREZ";
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" ;
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" ;
-
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-
-    if(mail($to,$subject,$message,$headers))
-    {
-        echo "Message accepted";
-    }
-    else
-    {
-        echo "Error: Message not accepted";
-    }
-
-    /*
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    */
-
+    $email = $_POST['Email'];
+    $message = $_POST['Message'];
+    $formcontent="From: $name \n Message: $message";
+    $recipient = "irborrero@gmail.com";
+    $subject = "Contact Form";
+    $mailheader = "From: $email \r\n";
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    echo "Thank You!";
     //header('Location: index0.html');
 
 }
