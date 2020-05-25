e726856445f2281569481de65dfbafaed2d435b6
Isabella Ramirez <irborrero@gmail.com>
2020-05-25 16:03:58 +0100

    trying sendgrid

--- contact.php
+++ contact.php
@@ -1,15 +1,21 @@
 <?php
 if(isset($_POST['submit'])){
 
-    $name = $_POST['Name'];
-    $email = $_POST['Email'];
-    $message = $_POST['Message'];
-    $formcontent="From: $name \n Message: $message";
-    $recipient = "irborrero@gmail.com";
-    $subject = "Contact Form";
-    $mailheader = "From: $email \r\n";
-    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-    echo "Thank You!";
+    require("sendgrid-php/sendgrid-php.php");
+
+    $from = new SendGrid\Email(null, "irborrero@gmail.com");
+    $subject = "Hello World from the SendGrid PHP Library!";
+    $to = new SendGrid\Email(null, "irborrero@gmail.com");
+    $content = new SendGrid\Content("text/plain", "Hello, Email!");
+    $mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+    $apiKey = getenv('SEke1vlhT_CH-yem_liXCw');
+    $sg = new \SendGrid($apiKey);
+
+    $response = $sg->client->mail()->send()->post($mail);
+    echo $response->statusCode();
+    echo $response->headers();
+    echo $response->body();
     //header('Location: index0.html');
 
 }
